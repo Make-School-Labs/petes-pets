d6f18493671e4adebef3a0bb0e51c453de4fec54
Jeffchiucp <jeffchiu2000@gmail.com>
2018-01-24 22:11:41 -0800

    adding comment and db

---
+++ db/models/pet.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Pet = sequelize.define('Pet', {
+    name: DataTypes.STRING,
+    species: DataTypes.STRING,
+    birthday: DataTypes.DATE,
+    favoriteFood: DataTypes.STRING,
+    picUrl: {type: DataTypes.STRING, validate: { isUrl: true } },
+    picUrlSq: {type: DataTypes.STRING, validate: { isUrl: true } },
+    description: DataTypes.TEXT
+  });
+
+  Pet.associate = function(models){
+      Pet.hasMany(models.Comment);
+  };
+  return Pet;
+};
