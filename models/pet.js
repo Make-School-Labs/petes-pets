32e143ff1cfe6a7aea72c766f9ae782f03cb1006
Jayce Azua <jayce.azua@gmail.com>
2019-01-27 16:23:15 -0800

    ...

--- models/pet.js
+++ models/pet.js
@@ -1,5 +1,3 @@
-"use strict";
-
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const mongoosePaginate = require('mongoose-paginate');
@@ -13,8 +11,9 @@ const PetSchema = new Schema({
   name: { type: String, required: [true, 'Give that little guy a name!'] }, 
   birthday: {type: String, required: [true, "I'm pretty sure the pup has a birthday"]},
   species: { type: String, required: [true, 'Uh-oh forgot the species'] },
-  picUrl: { type: String, required: [true, 'Needs a rectangular picture'] },
-  picUrlSq: { type: String, required: [true, 'Needs a square picture'] },
+  picUrl: { type: String },
+  picUrlSq: { type: String },
+  avatarUrl: { type: String, required: [true, 'Avatar photo failed to load or need there needs to be a photo']},
   favoriteFood: { type: String, required: [true, 'What about a favorite food?'] },
   description: { type: String, min: [100, 'Pets find homes faster with a good description.'] }
 }, {
--- routes/pets.js
+++ routes/pets.js
@@ -1,6 +1,35 @@
 // MODELS
 const petsRouter = require('express').Router();
 const Pet = require('../models/pet');
+// UPLOADING TO AWS S3
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+const Upload = require('s3-uploader');
+
+
+// Config S3
+const client = new Upload(process.env.S3_BUCKET, {
+  aws: {
+    path: 'pets/avatar',
+    region: process.env.S3_REGION,
+    acl: 'public-read',
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  },
+  cleanup: {
+    versions: true,
+    original: true
+  },
+  versions: [{
+    maxWidth: 400,
+    aspect: '16:10',
+    suffix: '-standard'
+  }, {
+    maxWidth: 300,
+    aspect: '1:1',
+    suffix: '-square'
+  }]
+});
 
 // PET ROUTES
 
@@ -12,14 +41,40 @@ petsRouter.get('/pets/new', (req, res) => {
 });
 
 // CREATE PET
-petsRouter.post('/pets', (req, res) => {
+petsRouter.post('/pets', upload.single('avatar'), (req, res, next) => {
+  
   let pet = new Pet(req.body);
-  pet.save()
-    .then((pet) => {
-      res.send({ pet });
-    })
-    .catch((err) => { res.status(400).send(err.errors) });
-});
+  // console.log(pet);
+  // next();
+  // res.redirect('/')
+  // pet.save(function (err) {
+  //   if (req.file) {
+  //     client.upload(req.file.path, {}, function (err, versions, meta) {
+  //       if (err) {
+  //         return res.status(400).send({
+  //           err: err
+  //         })
+  //       };
+
+  //       versions.forEach(function (image) {
+  //         var urlArray = image.url.split('-');
+  //         urlArray.pop();
+  //         var url = urlArray.join('-');
+  //         pet.avatarUrl = url;
+  //         pet.save();
+  //       });
+
+  //       res.send({
+  //         pet: pet
+  //       });
+  //     });
+  //   } else {
+  //     res.send({
+  //       pet: pet
+  //     });
+  //   }
+  // })
+})
 
 // EDIT PET
 petsRouter.get('/pets/:id/edit', (req, res) => {
